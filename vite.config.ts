f987eac897a506fb544086b4a5fa62238bf0702f
LorenzoDalBo <115959652+LorenzoDalBo@users.noreply.github.com>
2025-12-17 15:38:36 -0300

    Add base path to Vite configuration
    Add missing base path configuration for Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-template/",
+  base: "/portfolio-template/", // <--- Esta é a linha mágica que faltava
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
